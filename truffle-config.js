eb77755ff1091789b305caff1c05ef9b975deb99
kecan <1905537268@qq.com>
2025-06-05 00:01:54 +0800

    添加Truffle配置文件

---
+++ truffle-config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*",
+      gas: 6721975,
+      gasPrice: 20000000000
+    },
+    ganache: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*",
+      gas: 6721975,
+      gasPrice: 20000000000,
+      from: "0x0000000000000000000000000000000000000000"
+    }
+  },
+  contracts_directory: './contracts',
+  contracts_build_directory: './src/contracts',
+  migrations_directory: './migrations',
+  compilers: {
+    solc: {
+      version: "0.8.19",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200
+        }
+      }
+    }
+  }
+};
